ba991443e9866b08e1276dbe0d3ff40d326275bb
LiiiiiiiiY <1317262104@qq.com>
2024-01-03 23:35:33 +0800

    Program

---
+++ Java/Exam/Shape/Rectangle.java
@@ -0,0 +1,16 @@
+package Java.Exam.Shape;
+
+public class Rectangle extends Shapa {
+    
+    Rectangle(){
+        super();
+    }
+
+    public double gerArea(){
+        
+    }
+    
+    public double getLength(){
+
+    }
+}
---
+++ Java/Exam/Shape/Shape.java
@@ -0,0 +1,6 @@
+package Java.Exam.Shape;
+
+public abstract class Shape {
+    public abstract double gerArea();
+    public abstract double getLength();
+}
