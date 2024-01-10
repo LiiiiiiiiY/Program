34286c0a94c7e0a1be555154d0674b40a4cfe23c
LiiiiiiiiY <1317262104@qq.com>
2024-01-11 00:20:11 +0800

    Program

--- Java/Exam/Car/Test.java
+++ Java/Exam/Car/Test.java
@@ -8,10 +8,8 @@ public class Test {
         carList.add(new Car("changan", 120000));
         carList.add(new Car("byd", 230000));
         carList.add(new Car("geely", 140000));
-
         Comparator<Car> price = Comparator.comparingInt(Car::getter).reversed();
         Collections.sort(carList,price);
-
         for(Car car:carList){
             System.out.println(car);
         }
