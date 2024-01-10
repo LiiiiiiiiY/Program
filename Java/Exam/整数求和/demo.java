34db2c2fd7f4175f8235fe8242d05ae624e3afdd
LiiiiiiiiY <1317262104@qq.com>
2024-01-10 23:01:41 +0800

    Program

--- Java/Exam/整数求和/demo.java
+++ Java/Exam/整数求和/demo.java
@@ -1,20 +1,20 @@
 package Java.Exam.整数求和;
 
 import java.util.*;
-public class Test {
+public class demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int n = scanner.nextInt();
+        System.out.println(a);
+        System.out.println(n);
         scanner.close();
-        System.out.print(a);
-        int sum=a;
+        int sum = a;
         int b = a;
-        for(int i = 1; i < n; i++ ) {
+        for(int i=1;i<n;i++){
             a = b * (int)Math.pow(10,i) + a;
             sum += a;
-            System.out.print(" + "+ a);
         }
-        System.out.println(" = "+sum);
+        System.out.println(sum);
     }
 }
