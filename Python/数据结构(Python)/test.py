b8d3fa03f1c83e4b33e881beae2868d69f1b3645
LiiiiiiiiY <1317262104@qq.com>
2024-06-05 15:17:04 +0800

    Program

--- Python/数据结构(Python)/test.py
+++ Python/数据结构(Python)/test.py
@@ -1,60 +1,24 @@
-import numpy as np
-import jieba
-from tqdm import tqdm
-from docx import Document
-import re
+text = [
+    "40.html, 5, 3, 0.30, 1",
+    "04.html, 3, 1, 0.70, 1",
+    "66.html, 1, 2, 0.70, 1",
+    "62.html, 6, 3, 0.20, 1",
+    "52.html, 1, 4, 0.70, 1",
+    "32.html, 7, 3, 0.50, 0",
+    "26.html, 6, 3, 0.70, 1",
+    "61.html, 1, 3, 0.40, 1",
+    "71.html, 3, 4, 0.70, 0",
+    "34.html, 3, 4, 0.70, 1",
+    "11.html, 6, 4, 0.70, 0",
+    "91.html, 8, 4, 0.70, 1",
+    "45.html, 6, 3, 0.70, 1",
+    "81.html, 7, 3, 0.10, 0",
+    "01.html, 1, 1, 0.70, 1",
+]
 
-# 打开一个.docx文件
-doc = Document('学生手册.docx')
-
-# 读取并合并文档中所有段落的文本
-text = ' '.join([paragraph.text for paragraph in doc.paragraphs])
-# print(text)
-# 使用正则表达式根据中文标点符号切分文本
-# 这个正则表达式匹配了中文的句号、问号和感叹号
-pattern = r'。|？|！'
-
-# 使用re.split()进行切分，注意设置maxsplit参数以避免空字符串
-sentences = re.split(pattern, text)
-
-# 过滤掉空字符串（如果有的话）
-sentences = [sentence for sentence in sentences if sentence]
-
-# print(sentences)
-lines=sentences
-id2sent = dict(enumerate(lines))
-word2sentid = {}
-tf = {}
-
-for i in tqdm(iter(range(len(lines)-1))):
-    for w in set(jieba.cut(lines[i] + lines[i + 1],HMM=True)):
-        if w not in word2sentid:
-            word2sentid[w] = []
-        if w not in tf:
-            tf[w] = 0
-        word2sentid[w].append(i)
-        tf[w] += 1
-
-# 统计tf、idf的匹配
-total = sum(tf.values())*1.
-idf = {i:np.log(total)-np.log(j) for i,j in tf.items()}
-
-
-def answers(s):
-    ws = jieba.lcut(s, HMM=True)
-    result = {}
-    for w in ws:
-        if w in word2sentid:
-            for i in word2sentid[w]:
-                if i not in result:
-                    result[i] =0
-                result[i] += idf[w]
-
-    # 使用max函数和key参数找到最大值的索引
-    if result:
-        i = max(result, key=result.get)
-        return id2sent[i] + '\n' + id2sent[i + 1]
-    else:
-        return "没有找到相关的句子。"
-s=input("请输入查询内容:")
-print(answers(s))
+words = map(lambda l: l.split(","), text)
+counts = map(
+    lambda data: (data[0], round(float(data[1]) * 0.1 + float(data[2]) * 0.9 + float(data[3]) * 0.4 + float(data[4]),2)), words)
+counts = sorted(counts, key=lambda x: x[1], reverse=True)
+for item in counts:
+    print(item)
